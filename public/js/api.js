2754302e197f50f07584b41f6addc2b00536a017
Mithun1508 <mithuncoding@gmail.com>
2023-01-10 13:59:48 +0530

    first commit

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "8e02f5d334f7bf03082977ed2ebb9aba";
+
+let img_url = "https://image.tmdb.org/t/p/w500";
+let original_img_url = "https://image.tmdb.org/t/p/original";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+let movie_detail_http = "https://api.themoviedb.org/3/movie";
